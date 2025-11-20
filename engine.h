e3500ad4fb701e78687f91b09c24b0466179e116
lucasnoite <166355429+lucasnoite@users.noreply.github.com>
2025-11-20 19:06:30 -0400

    Update engine.h

--- engine.h
+++ engine.h
@@ -2,34 +2,40 @@
 #define ENGINE_H
 #define tipoitem char
 
-// Declaração da lista
-struct Node{
-    tipoitem *valor;
-    struct node *prox;
-}*lista;
-//Declaração da pilha
-typedef struct
-{
-    struct  node *topo;   
-}*pilha;
-//Essa função cria uma pilha
-pilha cria(void);
-//Essa função insere um valor da lista
-Node  insert_list(no *l, tipoitem x);
-//Essa munção remove o valor da lista
-Node remove_list( no *l);
-// Essa função insere um valor no topo
-void push(pilha *p, tipoitem x);
-//Essa função, verifica se a pilha está vazia
-int is_void( pilha *p);
-//Essa função ela remove o valor no topo
-tipoitem pop (pilha *p);
-// Essa função libera a pilha
-void livre(pilha*p);
-// Essa função impremi os valores
-void imprime(pilha* p);
 
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#define tipoitem char
+
+// Nó da lista
+typedef struct Node {
+    tipoitem valor;
+    struct Node *prox;
+} Node;
+
+// Pilha
+typedef struct {
+    Node *topo;
+} *pilha;
+
+// Funções
+pilha cria(void);
+Node* insert_list(Node *l, tipoitem x);
+Node* remove_list(Node *l);
+void push(pilha p, tipoitem x);
+int is_void(pilha p);
+tipoitem pop(pilha p);
+void livre(pilha p);
+void imprime(pilha p);
+
+int prioridade(char op);
+
+int infix_to_postfix(char infixa[], char posfixa[]);
+
+float avaliar_posfixa(char posfixa[], float valores[]); 
 
 
 #endif
+
 
