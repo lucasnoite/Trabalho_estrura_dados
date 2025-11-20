019a5b192e4b355908e3a9967e26a5b5410cd5a5
lucasnoite <166355429+lucasnoite@users.noreply.github.com>
2025-11-20 18:33:26 -0400

    Refactor engine.h for improved structure and clarity
    Refactor data structures and function signatures in engine.h

--- engine.h
+++ engine.h
@@ -1,35 +1,27 @@
 #ifndef ENGINE_H
 #define ENGINE_H
+
 #define tipoitem char
 
-// Declaração da lista
-struct Node{
-    tipoitem *valor;
-    struct node *prox;
-}*lista;
-//Declaração da pilha
-typedef struct
-{
-    struct  node *topo;   
-}*pilha;
-//Essa função cria uma pilha
+
+typedef struct Node {
+    tipoitem valor;
+    struct Node *prox;
+} Node;
+
+
+typedef struct {
+    Node *topo;
+} *pilha;
+
+
 pilha cria(void);
-//Essa função insere um valor da lista
-Node  insert_list(no *l, tipoitem x);
-//Essa munção remove o valor da lista
-Node remove_list( no *l);
-// Essa função insere um valor no topo
-void push(pilha *p, tipoitem x);
-//Essa função, verifica se a pilha está vazia
-int is_void( pilha *p);
-//Essa função ela remove o valor no topo
-tipoitem pop (pilha *p);
-// Essa função libera a pilha
-void livre(pilha*p);
-// Essa função impremi os valores
-void imprime(pilha* p);
-
-
+Node* insert_list(Node *l, tipoitem x);
+Node* remove_list(Node *l);
+void push(pilha p, tipoitem x);
+int is_void(pilha p);
+tipoitem pop(pilha p);
+void livre(pilha p);
+void imprime(pilha p);
 
 #endif
-
